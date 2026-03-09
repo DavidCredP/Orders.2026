b3b4fd9c86461fd9892abd96a73f6689e50c3466
DavidCredP <davidlopezarellanoo@gmail.com>
2026-03-09 15:03:03 -0800

    Add PUT and GET for countries

---
+++ Orders/Order.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Order.Backend.Data;
+using Orders.Shared.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Order.Backend.Controllers;
+
+[Route("api/[controller]")]
+
+[ApiController]
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CountriesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Countries.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Country country)
+    {
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+}
