50b19ae1595e2a1ef6caa44e2eee3e4045de1b00
DavidCredP <davidlopezarellanoo@gmail.com>
2026-03-19 15:40:19 -0800

    prodct classes finished, addedthe multipel selector in mudblazor, still is pending an fatal error in relation of the azure istore class

---
+++ Orders/Order.Frontend/Helpers/MultipleSelectorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Order.Frontend.Helpers;
+
+public class MultipleSelectorModel
+{
+    public MultipleSelectorModel(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public string Key { get; set; }
+
+    public string Value { get; set; }
+}
