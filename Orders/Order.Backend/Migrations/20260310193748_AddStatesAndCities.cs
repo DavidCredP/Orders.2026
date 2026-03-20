9271c0eac36bcc1460c20925d35214ebaea20999
DavidCredP <150203516+DavidCredP@users.noreply.github.com>
2026-03-20 12:37:33 -0700

    migration recovery

---
+++ Orders/Order.Backend/Migrations/20260310193748_AddStatesAndCities.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatesAndCities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryName",
+                table: "Countries",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Countries_CountryName",
+                table: "Countries",
+                newName: "IX_Countries_Name");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_CategoryName",
+                table: "Categories",
+                newName: "IX_Categories_Name");
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    StateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.StateId);
+                    table.ForeignKey(
+                        name: "FK_States_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityId);
+                    table.ForeignKey(
+                        name: "FK_Cities_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "StateId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities",
+                columns: new[] { "StateId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_States_CountryId_Name",
+                table: "States",
+                columns: new[] { "CountryId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "States");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Countries",
+                newName: "CountryName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                newName: "IX_Countries_CountryName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "CategoryName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                newName: "IX_Categories_CategoryName");
+        }
+    }
+}
